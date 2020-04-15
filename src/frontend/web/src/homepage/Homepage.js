2d565eff1338fdffabd44b1165c0169a7ad9d353
saifun <saifunny@gmail.com>
2020-04-15 15:43:52 +0300

    Added find a partner button

--- src/frontend/web/src/homepage/Homepage.js
+++ src/frontend/web/src/homepage/Homepage.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { Card } from 'antd';
-import {UsergroupAddOutlined} from '@ant-design/icons';
+import React from 'react';
+import { Button, Card } from 'antd';
+import { UsergroupAddOutlined } from '@ant-design/icons';
 
 import './homepage.scss';
 
 const Homepage = () => {
-    const chooseRandomImage = () =>
-        Math.floor(Math.random() * 2) === 0 ? '/images/male-stud.png' : '/images/female-stud.png' ;
+    const chooseRandomImage = () => (Math.floor(Math.random() * 2) === 0 ? '/images/male-stud.png' : '/images/female-stud.png');
 
     return (
         <div className="homepage-container">
-            <div className="data-container">
+            <div className="partner-request">
+                <Button type="primary" className="find-a-partner-button">
+                    אני רוצה למצוא שותף!
+                    <UsergroupAddOutlined />
+                </Button>
                 <img src={chooseRandomImage()} alt="Students" className="find-mate-image" />
-                <Card className="user-options-card">
-                    אני רוצה למצוא שותף! <UsergroupAddOutlined />
-                </Card>
             </div>
         </div>
     );
