d7868bd65134a3cccde80aba5f9c05d2f0eb5bd6
saifun <saifunny@gmail.com>
2020-04-15 21:29:31 +0300

    Added default drawer form

--- src/frontend/web/src/homepage/Homepage.js
+++ src/frontend/web/src/homepage/Homepage.js
@@ -1,19 +1,22 @@
 import React, { useContext } from 'react';
-import { Button } from 'antd';
+import { Button, Drawer } from 'antd';
 import { UsergroupAddOutlined } from '@ant-design/icons';
 import _ from 'lodash';
+import useToggle from 'react-use-toggle';
 
 import './homepage.scss';
 import { UserStoreContext } from '../user/UserStore';
+import AddCourseForm from './AddCourseForm';
 
 const Homepage = () => {
     const chooseRandomImage = () => (Math.floor(Math.random() * 2) === 0 ? '/images/male-stud.png' : '/images/female-stud.png');
     const { userMatches, userCourses } = useContext(UserStoreContext);
+    const [isCourseFormOpen, toggleCourseFormOpen] = useToggle(false);
 
     return (
         <div className="homepage-container">
             <div className="partner-request">
-                <Button type="primary" className="find-a-partner-button">
+                <Button type="primary" className="find-a-partner-button" onClick={toggleCourseFormOpen}>
                     אני רוצה למצוא שותף!
                     <UsergroupAddOutlined />
                 </Button>
@@ -25,6 +28,7 @@ const Homepage = () => {
             <div className="all-matches">
                 {_.map(userMatches, (match) => <p>{match}</p>)}
             </div>
+            <AddCourseForm isDrawerOpen={isCourseFormOpen} toggleDrawer={toggleCourseFormOpen} />
         </div>
     );
 };
