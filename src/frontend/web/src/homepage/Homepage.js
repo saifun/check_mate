75a521c102167be768a189513431432a20b4bbb1
saifun <saifunny@gmail.com>
2020-04-15 14:43:51 +0300

    Added student img

--- src/frontend/web/src/homepage/Homepage.js
+++ src/frontend/web/src/homepage/Homepage.js
@@ -5,13 +5,19 @@ import {UsergroupAddOutlined} from '@ant-design/icons';
 import './homepage.scss';
 
 const Homepage = () => {
-    return (<div className="homepage-container">
-        <div className="data-container">
-            <Card className="user-options-card">
-                אני רוצה למצוא שותף! <UsergroupAddOutlined />
-            </Card>
+    const chooseRandomImage = () =>
+        Math.floor(Math.random() * 2) === 0 ? '/images/male-stud.png' : '/images/female-stud.png' ;
+
+    return (
+        <div className="homepage-container">
+            <div className="data-container">
+                <img src={chooseRandomImage()} alt="Students" className="find-mate-image" />
+                <Card className="user-options-card">
+                    אני רוצה למצוא שותף! <UsergroupAddOutlined />
+                </Card>
+            </div>
         </div>
-    </div>);
+    );
 };
 
 export default Homepage;
